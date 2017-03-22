3ac6565dfad518be5121f62cc8dd4ddec9af03e0
bunyawat <bunyawat.38@gmail.com>
2017-03-22 22:34:27 +0700

    main function finished

--- src/pkg56070501071ex08_2/Main.java
+++ src/pkg56070501071ex08_2/Main.java
@@ -15,30 +15,60 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
 
 public class Main extends Application {
     ImageView[][] ticPos = new ImageView[3][3];
+    ImageView playerSideImage = new ImageView();
+    TicTac games = new TicTac();
+
+    File circleImageFile = new File("circle.png");
+    File crossImageFile = new File("cross.png");
+    File squareImageFile = new File("square.png");
+
+    Image circleImage;
+    Image crossImage;
+    Image squareImage;
+
+    Alert annoucer = new Alert(Alert.AlertType.INFORMATION);
+    Alert initMessage = new Alert(Alert.AlertType.INFORMATION);
+    ButtonType tryAgainButton = new ButtonType("Try Again");
+    ButtonType start1 = new ButtonType("Start as X");
+    ButtonType start2 = new ButtonType("Start as O");
+    ButtonType closeButton = new ButtonType("Close");
+    
+    private static final Integer STARTTIME = 8;
+    private Timeline timeline;
+    private IntegerProperty timeSeconds = new SimpleIntegerProperty(STARTTIME*100);
     
     @Override
     public void start(Stage primaryStage) {
-        Alert initMessage = new Alert(Alert.AlertType.INFORMATION);
-        ButtonType start1 = new ButtonType("Start as X");
-        ButtonType start2 = new ButtonType("Start as O");
-        ButtonType cancel = new ButtonType("Cancel");
         initMessage.setTitle("Welcome message");
         initMessage.setHeaderText("Welcome To TIC-TAC-TOE 2017");
-        initMessage.setContentText("Instruction\nEach turn have a time limit. hen ready, pick your side.");
-        initMessage.getButtonTypes().setAll(start1, start2, cancel);
+        initMessage.setContentText("Instruction\nEach turn have a time limit.\n When ready, pick 1st player's side.");
+        initMessage.getButtonTypes().setAll(start1, start2, closeButton);
+
+        annoucer.setTitle("Congratulation!");
+        annoucer.getButtonTypes().setAll(tryAgainButton, closeButton);
         
         SplitPane root = new SplitPane();
         GridPane leftView = new GridPane();
-        
-        File circleImage= new File("circle.png");
-        Image image = new Image(circleImage.toURI().toString());
+
+        circleImage = new Image(circleImageFile.toURI().toString());
+        crossImage = new Image(crossImageFile.toURI().toString());
+        squareImage = new Image(squareImageFile.toURI().toString());
         
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                ticPos[i][j] = new ImageView(image);
+                ticPos[i][j] = new ImageView(squareImage);
                 ticPos[i][j].setPickOnBounds(true);
                 leftView.add(ticPos[i][j], i, j);
             }
@@ -48,19 +78,54 @@ public class Main extends Application {
             node.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
-                    System.out.println("Clicked On (" + GridPane.getColumnIndex(node) + ", " + GridPane.getRowIndex(node)+ ")");
+                    int col = GridPane.getColumnIndex(node);
+                    int row = GridPane.getRowIndex(node);
+                    if(games.setHole(games.getCurrentPlayer(), col, row)){
+                        int winner = games.checkIfWin();
+                        games.swapCurrentPlayer();
+                        updateImage();
+                        if(winner!=-1){
+                            String winnerAnnounce = "Player " + winner + " has won";
+                            annoucer.setHeaderText(winnerAnnounce);
+                            annoucer.setContentText("Dear Loser...\nTRY AGAIN OR SURRENDER(Close)");
+                            Optional<ButtonType> loserResult = annoucer.showAndWait();
+                            if(loserResult.get()==tryAgainButton){
+                                restartPane(primaryStage);
+                            }
+                            else{
+                                primaryStage.close();
+                            }
+                        }
+                        else{
+                            if(games.checkIfDraw()){
+                            Alert drawAlert = new Alert(Alert.AlertType.INFORMATION);
+                            drawAlert.setHeaderText("DRAW");
+                            drawAlert.setContentText("Try again");
+                            drawAlert.showAndWait();
+                            restartPane(primaryStage);
+
+                        }
+                        }
+                    }
                 }
             });
         }
         
         VBox rightView = new VBox();
+        
+        ProgressBar progressBar = new ProgressBar();
+        progressBar.progressProperty().bind(
+                timeSeconds.divide(STARTTIME*100.0).subtract(1).multiply(-1));
+        
+        rightView.getChildren().add(progressBar);
+        
+        Label playerTurnLabel = new Label("Player's turn");
+        rightView.getChildren().add(playerTurnLabel);
+        rightView.getChildren().add(playerSideImage);
+        root.setDividerPositions(0.7f, 0.3f);
         root.getItems().add(leftView);
         root.getItems().add(rightView);
-       
-       
-        //root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 900, 500);
+        Scene scene = new Scene(root, 600, 450);
         
         primaryStage.setTitle("Tic Tac Toe 2017 - By Banyawat");
         primaryStage.setScene(scene);
@@ -68,21 +133,82 @@ public class Main extends Application {
         primaryStage.show();
         
         if(welcomeResult.get() == start1){
-            System.out.println("Start as X");
-            TicTac games = new TicTac();
-            games.checkIfWin();
+            games.setCurrentPlayer(0);
+            playerSideImage.setImage(crossImage);
+            startTimer();
         }
         else if(welcomeResult.get() == start2){
-            
+            games.setCurrentPlayer(1);
+            playerSideImage.setImage(circleImage);
         }
         else{
             primaryStage.close();
         }
-        
+    }
+
+    private void updateImage(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                int val = games.getHole(i,j);
+                if(val==0){
+                    ticPos[i][j].setImage(crossImage);
+                }
+                else if(val==1){
+                    ticPos[i][j].setImage(circleImage);
+                }
+                else{
+                    ticPos[i][j].setImage(squareImage);
+                }
+            }
+        }
+
+        if(games.getCurrentPlayer()==0)
+            playerSideImage.setImage(crossImage);
+        else
+            playerSideImage.setImage(circleImage);
+    }
+
+    private void restartPane(Stage primaryStage){
+        primaryStage.close();
+        initMessage.setTitle("Welcome message");
+        initMessage.setHeaderText("Welcome To TIC-TAC-TOE 2017");
+        initMessage.setContentText("Instruction\nEach turn have a time limit.\n When ready, pick 1st player's side.");
+        games = new TicTac();
+        Optional<ButtonType> welcomeResult = initMessage.showAndWait();
+        updateImage();
+        primaryStage.show();
+        if(welcomeResult.get() == start1){
+            games.setCurrentPlayer(0);
+            playerSideImage.setImage(crossImage);
+        }
+        else if(welcomeResult.get() == start2){
+            games.setCurrentPlayer(1);
+            playerSideImage.setImage(circleImage);
+        }
+        else{
+            primaryStage.close();
+        }
+    }
+    
+    private void startTimer(){
+        if (timeline != null) {
+                    timeline.stop();
+                }
+                timeSeconds.set((STARTTIME+1)*100);
+                timeline = new Timeline();
+                timeline.getKeyFrames().add(
+                        new KeyFrame(Duration.seconds(STARTTIME+1),
+                        new KeyValue(timeSeconds, 0)));
+                timeline.setOnFinished(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        System.out.println("Time's up");
+                    }
+                });
+                timeline.playFromStart();
     }
 
     public static void main(String[] args) {
         launch(args);
-    }
-    
+    }    
 }
--- src/pkg56070501071ex08_2/TicTac.java
+++ src/pkg56070501071ex08_2/TicTac.java
@@ -2,49 +2,97 @@ package pkg56070501071ex08_2;
 
 public class TicTac {
     private int[][] holes = new int[3][3];
+    private int currentPlayer=0;
     
     public TicTac(){
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
                 holes[i][j]=-1;
-                if(i==2)
-                    /*if(j==1)
-                        holes[i][j]=1;
-                    else*/
-                        holes[i][j]=1;
             }
         }
         
+        printCell();
+    }
+
+    public void setCurrentPlayer(int player){
+        this.currentPlayer=player;
+    }
+
+    public void swapCurrentPlayer(){
+        if(this.currentPlayer==0)
+            this.currentPlayer=1;
+        else
+            this.currentPlayer=0;
+    }
+
+
+    public int getCurrentPlayer(){
+        return this.currentPlayer;
+    }
+    
+    public boolean setHole(int val, int col, int row){
+        if(holes[row][col]==-1){
+            holes[row][col]=val;
+            return true;
+        }
+        return false;
+    }
+    
+    public int getHole(int col, int row){
+        return holes[row][col];
+    }
+    
+    public int checkIfWin(){
+        for(int i=0;i<3;i++){
+            if((holes[i][0]==holes[i][1])&&(holes[i][1]==holes[i][2])){
+                if(holes[i][0]!=-1){
+                    System.out.println("Win");
+                    return currentPlayer;
+                }
+            }
+            
+            if((holes[0][i]==holes[1][i])&&(holes[1][i]==holes[2][i])){
+                if(holes[0][i]!=-1){
+                    System.out.println("Win");
+                    return currentPlayer;
+                }
+            }
+        }
+
+        //Check cross condition
+        if((holes[0][0]==holes[1][1])&&(holes[1][1]==holes[2][2])){
+            if(holes[0][0]!=-1){
+                System.out.println("Win");
+                return currentPlayer;
+            }
+        }
+
+        if((holes[2][0]==holes[1][1])&&(holes[1][1]==holes[0][2])){
+            if(holes[2][0]!=-1){
+                System.out.println("Win");
+                return currentPlayer;
+            }
+        }
+
+        return -1;
+    }
+
+    public boolean checkIfDraw(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(holes[i][j]==-1)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public void printCell(){
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
                 System.out.print("[" + holes[i][j] +"]\t");
             }
             System.out.println();
-        }
-    }
-    
-    public void setHole(int val, int col, int row){
-        if(holes[col][row]==0){
-            holes[col][row]=val;
-        }
-    }
-    
-    public int getHole(int col, int row){
-        return holes[col][row];
-    }
-    
-    public void checkIfWin(){
-        for(int i=0;i<3;i++){
-
-            if((holes[i][0]==holes[i][1])&&(holes[i][1]==holes[i][2])){
-                if(holes[i][0]!=-1)
-                    System.out.println("Win");
-            }
-            
-            if((holes[0][i]==holes[1][i])&&(holes[1][i]==holes[2][i])){
-                if(holes[0][i]!=-1)
-                    System.out.println("Win");
-            }
         }
     }
 }
